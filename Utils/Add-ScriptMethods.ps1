f0650e46570dda5f46f023d590c3e224272c35c9
Blue Falcon <130698314+anonhostpi@users.noreply.github.com>
2025-02-08 17:28:32 -0700

    Add-ScriptMethods.ps1

---
+++ Utils/Add-ScriptMethods.ps1
@@ -0,0 +1,14 @@
+function global:Add-ScriptMethods {
+    param(
+        [psobject] $target,
+        [System.Collections.IDictionary] $Members
+    )
+
+    $keys = $Members.Keys | ForEach-Object { $_ }
+
+    If( $keys.Count ){
+        $keys | ForEach-Object {
+            $target | Add-Member -MemberType ScriptMethod -Name $_ -Value $Members[$_] -Force
+        }
+    }
+}
