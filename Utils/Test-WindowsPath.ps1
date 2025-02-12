89fc3616bfcbb8dd8a95ff4ad4e652d544ff2e68
Blue Falcon <130698314+anonhostpi@users.noreply.github.com>
2025-02-12 02:08:30 -0700

    add Test-WindowsPath.ps1

---
+++ Utils/Test-WindowsPath.ps1
@@ -0,0 +1,19 @@
+function global:Test-WindowsPath {
+    param (
+        [string] $Path
+    )
+
+    if ($Path -match '^(?:([a-zA-Z]:))') {
+        If( -not (Test-Path $matches[1]) ){             return $false }
+    }
+
+    If( -not ($Path -match '[^\<\>\:"\|\?\*\n\r]') ){   return $false }
+
+    $segments = $Path -split "[\\\/]+"
+    foreach( $segment in $segments ){
+        If( $segment.StartsWith(" ") ){                 return $false }
+        If( $segment.EndsWith(" ") ){                   return $false }
+        If( (-not ($segment -match "^\.{1,2}$")) -and $segment.EndsWith(".") ){                   return $false }
+    }
+                                                        return $true
+}
